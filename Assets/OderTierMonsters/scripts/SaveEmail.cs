1857549c7ae368e529b0903cf18bac950c9533a3
unknown <dennydenii@gmail.com>
2020-05-20 01:07:43 +0100

    Verification for email format to enable button

--- Assets/OderTierMonsters/scripts/SaveEmail.cs
+++ Assets/OderTierMonsters/scripts/SaveEmail.cs
@@ -3,23 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Net.Mail;
+using System;
+using System.Text;
+using System.Text.RegularExpressions;
 
-public class SaveEmail : MonoBehaviour {
+public class SaveEmail : MonoBehaviour
+{
+	public const string MatchEmailPattern =
+            @"^(([\w-]+\.)+[\w-]+|([a-zA-Z]{1}|[\w-]{2,}))@"
+            + @"((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\.([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\."
+              + @"([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\.([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])){1}|"
+            + @"([a-zA-Z]+[\w-]+\.)+[a-zA-Z]{2,4})$";
 
-	public Button nextBtn;
-	public void saveUserEmail(string input){
-		SimpleEmailSender.SetEmail(input);
-	}	
+    public Button nextBtn;
 
-	public void EnableBtnNext(InputField input){
-		saveUserEmail(input.text);
-		if(input.text != "")
+    public void EnableBtnNext(InputField input)
+    {  
+        if(IsEmail(input.text)){
 			nextBtn.interactable = true;
-		else
+		}
+		else{
 			nextBtn.interactable = false;
-	}
+		}   
+    }
 
-	public void StartGame(){
-		SceneManager.LoadScene("OrderTierMonsters");
-	}
+	    public void saveUserEmail(string input)
+    {
+        SimpleEmailSender.SetEmail(input);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("OrderTierMonsters");
+    }
+
+	public static bool IsEmail(string email)
+        {
+            if (email != null) 
+				return Regex.IsMatch(email, MatchEmailPattern);
+            else 
+				return false;
+        }
 }
