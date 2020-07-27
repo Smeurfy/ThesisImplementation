29f43ddfbc7ee5ed6e45cf4cf0867f513b6da9bc
unknown <dennydenii@gmail.com>
2020-07-27 23:26:50 +0100

    added the ID to the final scene

--- Assets/Acknowledgement screen/AcknowledgementScreen.cs
+++ Assets/Acknowledgement screen/AcknowledgementScreen.cs
@@ -1,8 +1,10 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
-public class AcknowledgementScreen : MonoBehaviour 
+public class AcknowledgementScreen : MonoBehaviour
 {
+    public TextMeshProUGUI _text;
     public void CloseGame()
     {
         Application.Quit();
@@ -11,5 +13,10 @@ public class AcknowledgementScreen : MonoBehaviour
     public void BackToMainMenu()
     {
         SceneManager.LoadScene("main menu");
+    }
+
+    private void OnEnable()
+    {
+        _text.text = "Your ID is: " + PlayerPrefs.GetString("playerID");
     }
 }
