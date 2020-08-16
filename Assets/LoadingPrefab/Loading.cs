0f1162ecf14be9dee0bce85ae6ab3c1b4e2f30e5
unknown <dennydenii@gmail.com>
2020-08-16 22:02:50 +0100

    added animation to loading. new IST foto

---
+++ Assets/LoadingPrefab/Loading.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Loading : MonoBehaviour
+{
+	Text _text;
+    private void Start()
+    {
+		_text = GetComponentInChildren<Text>();
+		StartCoroutine(AnimText());
+    }
+
+    private IEnumerator AnimText()
+    {
+		yield return new WaitForSecondsRealtime(UnityEngine.Random.Range(0f, 1f));
+		if(_text.text == "Loading...")
+			_text.text = "Loading";
+		else
+			_text.text += ".";
+		StartCoroutine(AnimText());
+    }
+}
